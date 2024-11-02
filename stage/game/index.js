9400b0baa7bda7b9ad2a35d5cf599e8207da40ca
w-t-w <15866369958@163.com>
2024-11-02 19:02:54 +0800

    feature: 认识 nodejs API 以及搭建最简单的石头剪刀布游戏

--- stage/game/index.js
+++ stage/game/index.js
@@ -1,23 +1,25 @@
-const argv = process.argv,
-    length = argv.length;
+const params = process.argv,
+    length = params.length;
 let position = 0;
 
-const playerAction = argv[length - 1];
+const playerAction = params[length - 1];
 
 const computerRandom = Math.floor(Math.random() * 3);
-const gameSelections = ["scissor", "rock", "paper"];
 
-position = computerRandom + 1;
-if(position === length) position = 0;
+const gameSelections = ["scissor", "rock", "paper"];
 
 const computerAction = gameSelections[computerRandom];
 
+position = computerRandom + 1;
+if (position === length) position = 0;
+
 console.log(`玩家: ${playerAction}, 电脑: ${computerAction}`);
-if(playerAction === computerAction) {
+console.log("结果: ");
+
+if (playerAction === computerAction) {
     console.log("平局!");
 } else if (gameSelections[position] === playerAction) {
     console.log("玩家赢!电脑输!");
 } else {
     console.log("电脑赢!玩家输!");
 }
-
