5382296b85cd0f2e1f747b6893d7a684f2c8a99d
w-t-w <15866369958@163.com>
2024-11-04 10:08:43 +0800

    feature: 优化处理遵从 commonjs 规范的石头、剪刀、布猜拳游戏

--- stage/nonBlockingIO/nonBlockIO.js
+++ stage/nonBlockingIO/nonBlockIO.js
@@ -5,7 +5,7 @@ let result = null;
 console.time('glob');
 glob(`${process.cwd()}/**/*`, (err, data) => {
     result = data;
-    console.log('got result!');
+    console.log('got result:', result);
 });
 console.timeEnd('glob');
 console.log(100 + 1);
