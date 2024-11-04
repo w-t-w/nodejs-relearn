2bd8e3ff8dfc7fbc7f2492c64f3530936282934b
w-t-w <15866369958@163.com>
2024-11-04 10:51:44 +0800

    feature: callback 回调函数

--- stage/callback/index.js
+++ stage/callback/index.js
@@ -1,12 +1,43 @@
-interview(function (result) {
-    if (result) {
-        console.log("smile");
+// interview(function (result) {
+//     if (result) {
+//         console.log("smile");
+//     }
+// })
+//
+// function interview(callback) {
+//     const probability = Math.random();
+//     const timer = setTimeout(function () {
+//         if (probability < 0.8) {
+//             callback("success");
+//             clearTimeout(timer);
+//         }
+//     }, 500);
+// }
+// try {
+// interview(function (err, result) {
+//     if (err) {
+//         return console.error('cry!', err);
+//     }
+//     console.log('smile!', result);
+// });
+// } catch (err) {
+//     console.error('cry!', err);
+// }
+interview(function (err, result) {
+    if (err) {
+        return console.error('cry!', err);
     }
-})
+    console.log('smile!', result);
+});
 
 function interview(callback) {
     const probability = Math.random();
-    if (probability < 0.8) {
-        callback("success");
-    }
+    const timer = setTimeout(function () {
+        if (probability < 0.6) {
+            callback(null, "success!")
+        } else {
+            callback(new Error("failed!"));
+        }
+        clearTimeout(timer);
+    }, 500);
 }
