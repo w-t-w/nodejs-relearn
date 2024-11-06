8c33452360d51f9c16f818122db8ee3ce4af7d19
w-t-w <15866369958@163.com>
2024-11-06 14:46:59 +0800

    feature: 优化处理 koa 服务开发在页面上的石头、剪刀和布的游戏,验证不可处理异步事件

--- stage/gameKoa/gameKoa.js
+++ stage/gameKoa/gameKoa.js
@@ -16,13 +16,23 @@ const resultConfig = {
     1: "你赢了!"
 };
 
-gameKoa.use(function ({request, response}, next) {
-    const {action} = request.query;
+gameKoa.use(async function (ctx, next) {
+    const {response} = ctx;
+
     if (playerWonCount >= 3 || errorCode === 9) {
         response.status = 500;
         response.body = "你太厉害了!我不跟你玩儿了!";
         return false;
     }
+
+    await next();
+
+    if (ctx.playerWon) playerWonCount++;
+});
+
+gameKoa.use(async function (ctx) {
+    const {request, response} = ctx;
+    const {action} = request.query;
 
     if (!action || playerSameCount >= 3) {
         response.status = 400;
@@ -38,12 +48,19 @@ gameKoa.use(function ({request, response}, next) {
     }
     playerLastAction = action;
 
-    const result = game(action);
+    await new Promise(function (resolve) {
+        const timer = setTimeout(function () {
+            const result = game(action);
 
-    if (playerWonCode === result) playerWonCount++;
+            // if (playerWonCode === result) playerWonCount++;
+            if (playerWonCode === result) ctx.playerWon = true;
 
-    response.status = 200;
-    response.body = resultConfig[result];
+            response.status = 200;
+            response.body = resultConfig[result];
+            resolve();
+            clearTimeout(timer);
+        }, 100);
+    });
 });
 
 module.exports = gameKoa;
